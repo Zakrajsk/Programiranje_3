52306eceecb269a9a103764702ff8272f6c3d8c9
Zakrajsk <gal.zakrajsek@gmail.com>
2022-02-14 13:44:38 +0100

    Dodane dodatne naloge iz vaj1

---
+++ Vaje_01/Kattis-ABC/Program.cs
@@ -0,0 +1,37 @@
+﻿//Link: https://open.kattis.com/problems/abc
+
+using System;
+
+namespace Kattis_ABC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stevila = Console.ReadLine();
+            char[] crke = Console.ReadLine().ToCharArray();
+
+            int[] tab_stevil = Array.ConvertAll(stevila.Split(' '), int.Parse);
+            
+            Array.Sort(tab_stevil);
+
+            string rezultat = "";
+            for (int i = 0; i < 3; i++)
+            {
+                switch (crke[i])
+                {
+                    case 'A': rezultat += tab_stevil[0]; break;
+                    case 'B': rezultat += tab_stevil[1]; break;
+                    case 'C': rezultat += tab_stevil[2]; break;
+
+                }
+
+                if (i != 2)
+                {
+                    rezultat += " ";
+                }
+            }
+            Console.WriteLine(rezultat);
+        }
+    }
+}
---
+++ Vaje_01/Kattis-Above_Average/Program.cs
@@ -0,0 +1,39 @@
+﻿//Link: https://open.kattis.com/problems/aboveaverage
+
+using System;
+
+namespace Kattis_Above_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stevilo_primerov = int.Parse(Console.ReadLine());
+            for (int i = 0; i < stevilo_primerov; i++)
+            {
+                string[] podatki = Console.ReadLine().Split(' ');
+
+                int st_studentov = int.Parse(podatki[0]);
+                int vsota_ocen = 0;
+                for (int j = 1; j <= st_studentov; j++)
+                {
+                    vsota_ocen += int.Parse(podatki[j]);
+                }
+                double povprecje = (double)vsota_ocen / st_studentov;
+                int st_nadpovprecnih = 0;
+
+                for (int j = 1; j <= st_studentov; j++)
+                {
+                    if (int.Parse(podatki[j]) > povprecje)
+                    {
+                        st_nadpovprecnih++;
+                    }
+                }
+
+                double odstotek_nadpovprecnih = (double)st_nadpovprecnih / st_studentov;
+                //String format poskrbi, ce se pojavi premalo decimalk, da jih nadomesti z 0, saj naloga tako zahteva v kattisu
+                Console.WriteLine($"{String.Format("{0:0.000}", Math.Round(odstotek_nadpovprecnih * 100, 3))}%");
+            }
+        }
+    }
+}
